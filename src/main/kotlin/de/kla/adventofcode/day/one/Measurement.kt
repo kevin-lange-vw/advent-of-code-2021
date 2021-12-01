172154a8c5a269a87f88e851be32d187bce853c4
Kevin Lange <kevin.lange2@volkswagen.de>
2021-12-01 10:56:37 +0100

    day one part two

--- src/main/kotlin/de/kla/adventofcode/day/one/Measurement.kt
+++ src/main/kotlin/de/kla/adventofcode/day/one/Measurement.kt
@@ -3,6 +3,11 @@ package de.kla.adventofcode.day.one
 import java.io.File
 
 fun main() {
+    individualIncreases()
+    sumIncreases()
+}
+
+fun individualIncreases() {
     var increasedCounter = 0
     var lastNumber = Int.MAX_VALUE
 
@@ -13,7 +18,33 @@ fun main() {
         }
         lastNumber = currentNumber
     }
-    println(increasedCounter)
+    println("individual increases:$increasedCounter")
+}
+
+fun sumIncreases() {
+    val twoLineOffset = 2
+    var increasedCounter = 0 - twoLineOffset
+
+    var number1 = 0
+    var number2 = 0
+    var number3 = 0
+
+    var lastSum = Int.MAX_VALUE
+
+    inputFile().forEachLine {
+        val currentNumber = it.toInt()
+
+        number3 = number2
+        number2 = number1
+        number1 = currentNumber
+
+        val currentSum = number1 + number2 + number3
+        if (lastSum < currentSum) {
+            increasedCounter++
+        }
+        lastSum = currentSum
+    }
+    println("sum increases:$increasedCounter")
 }
 
 class Measurement
