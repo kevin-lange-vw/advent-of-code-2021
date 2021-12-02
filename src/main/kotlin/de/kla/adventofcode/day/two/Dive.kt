d458320a03bd8ec81a4431570d477bd66bd9ec8e
Kevin Lange <kevin.lange2@volkswagen.de>
2021-12-02 16:26:28 +0100

    day two part two

--- src/main/kotlin/de/kla/adventofcode/day/two/Dive.kt
+++ src/main/kotlin/de/kla/adventofcode/day/two/Dive.kt
@@ -1,17 +1,26 @@
 package de.kla.adventofcode.day.two
 
 import de.kla.adventofcode.helper.inputFile
+import java.io.File
 import kotlin.math.absoluteValue
 
+val EXAMPLE_FILE = inputFile("example-2.txt")
+val INPUT_FILE = inputFile("input-2.txt")
+
 fun main() {
-    partOne()
+    println("EXAMPLE:")
+    partOne(EXAMPLE_FILE)
+    partTwo(EXAMPLE_FILE)
+    println("REAL:")
+    partOne(INPUT_FILE)
+    partTwo(INPUT_FILE)
 }
 
-private fun partOne() {
+private fun partOne(file: File) {
     var depth = 0
     var distance = 0
 
-    inputFile("input-2.txt").forEachLine {
+    file.forEachLine {
         if (it.startsWith("forward ")) {
             distance += it.substringAfter("forward ").toInt()
         }
@@ -24,4 +33,26 @@ private fun partOne() {
     }
 
     println("part One: ${depth.absoluteValue * distance}")
+}
+
+private fun partTwo(file: File) {
+    var depth = 0
+    var distance = 0
+    var aim = 0
+
+    file.forEachLine {
+        if (it.startsWith("forward ")) {
+            val number = it.substringAfter("forward ").toInt()
+            distance += number
+            depth += (number * aim)
+        }
+        if (it.startsWith("up ")) {
+            aim -= it.substringAfter("up ").toInt()
+        }
+        if (it.startsWith("down ")) {
+            aim += it.substringAfter("down ").toInt()
+        }
+    }
+
+    println("part two: ${depth.absoluteValue * distance}")
 }
